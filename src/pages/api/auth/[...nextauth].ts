62bb46f1acde8f1ccd11452f5571974a0e0e6da6
joe.lai <ao6g4ru042k7xd@gmail.com>
2023-03-01 16:41:25 +0800

    feat: keycloak env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,9 +16,12 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     secret: NEXT_PUBLIC_SESSION_SECRET,
     providers: [
       KeycloakProvider({
-        clientId: process.env.KEYCLOAK_ID,
-        clientSecret: process.env.KEYCLOAK_SECRET,
-        issuer: process.env.KEYCLOAK_ISSUER,
+        // clientId: process.env.KEYCLOAK_ID,
+        // clientSecret: process.env.KEYCLOAK_SECRET,
+        // issuer: process.env.KEYCLOAK_ISSUER,
+        clientId: 'sfas',
+        clientSecret: 'f8eacvc6D1L9qPaijPSjtRc2IHwjA5Jt',
+        issuer: 'https://accounts.tokenbricks-dev.com/realms/tokenbricks',
         profile: (profile) => ({
           ...profile,
           id: profile.sub,
